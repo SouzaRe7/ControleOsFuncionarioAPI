8dff928b6497dcd82cb66247c1f386bf646d2c08
Renan Siqueira de Souza <souzare7@outlook.com>
2023-02-23 23:33:52 -0300

    Update do composer

--- src/resource/js/funcoes.js
+++ src/resource/js/funcoes.js
@@ -78,6 +78,8 @@ function GetTnkValue() {
     var token = GetTnk();
     var base64Url = token.split('.')[1];
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    console.log(base64Url); // Adicionar essa linha
+    console.log(base64); 
     var j = decodeURIComponent(window.atob(base64).split('').map(function (c) {
     return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
